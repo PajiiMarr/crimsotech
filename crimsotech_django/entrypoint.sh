7f485945f175bc1ae1a07cc8b175e3470118681e
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2026-01-24 20:33:49 +0800

    Dev server added in deployment shell

--- crimsotech_django/entrypoint.sh
+++ crimsotech_django/entrypoint.sh
@@ -7,13 +7,16 @@ set -ex
 # echo "==> Collecting static files..."
 # python manage.py collectstatic --noinput || true
 
-echo "==> Starting Gunicorn server on port 8000..."
-exec gunicorn backend.wsgi:application \
-    --bind 0.0.0.0:8000 \
-    --workers 1 \
-    --timeout 600 \
-    --log-level debug \
-    --capture-output \
-    --enable-stdio-inheritance \
-    --access-logfile - \
-    --error-logfile -
+# echo "==> Starting Gunicorn server on port 8000..."
+# exec gunicorn backend.wsgi:application \
+#     --bind 0.0.0.0:8000 \
+#     --workers 1 \
+#     --timeout 600 \
+#     --log-level debug \
+#     --capture-output \
+#     --enable-stdio-inheritance \
+#     --access-logfile - \
+#     --error-logfile -
+
+echo "==> Testing with Django dev server..."
+python manage.py runserver 0.0.0.0:8000
