8a3c1c5e7c6c9a6ddfdca083c775b09dd878871c
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2026-01-24 20:43:20 +0800

    removed gunicorn

--- crimsotech_django/entrypoint.sh
+++ crimsotech_django/entrypoint.sh
@@ -1,22 +1,5 @@
 #!/bin/sh
 set -ex
 
-# echo "==> Running migrations..."
-# python manage.py migrate --noinput
-
-# echo "==> Collecting static files..."
-# python manage.py collectstatic --noinput || true
-
-# echo "==> Starting Gunicorn server on port 8000..."
-# exec gunicorn backend.wsgi:application \
-#     --bind 0.0.0.0:8000 \
-#     --workers 1 \
-#     --timeout 600 \
-#     --log-level debug \
-#     --capture-output \
-#     --enable-stdio-inheritance \
-#     --access-logfile - \
-#     --error-logfile -
-
 echo "==> Testing with Django dev server..."
 python manage.py runserver 0.0.0.0:8000
