ba9a558b9b7238b950ca3e1076cd520017bb70bf
PajiiMarr <151644959+PajiiMarr@users.noreply.github.com>
2026-01-25 19:09:18 +0800

    entry server added

---
+++ crimsotech-app/app/entry.server.tsx
@@ -0,0 +1,30 @@
+import type { AppLoadContext, EntryContext } from "react-router";
+import { ServerRouter } from "react-router";
+import { renderToString } from "react-dom/server";
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  routerContext: EntryContext,
+  loadContext: AppLoadContext
+) {
+  const html = renderToString(
+    <ServerRouter context={routerContext} url={request.url} />
+  );
+
+  return new Response("<!DOCTYPE html>" + html, {
+    headers: { "Content-Type": "text/html" },
+    status: responseStatusCode,
+  });
+}
+
+export function getLoadContext(): AppLoadContext {
+  const contextStore = new Map();
+  
+  return {
+    // Polyfill for context.get() and context.set()
+    get: (key: any) => contextStore.get(key),
+    set: (key: any, value: any) => contextStore.set(key, value),
+  };
+}
